83e303e3f8aac170862e567dd7ae0a7030cab6db
Abeeb Raheem (Beloved) <66231101+belovetech@users.noreply.github.com>
2020-11-19 11:19:33 +0000

    Ading fies for gihub pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ constructor() {
+   super();
+
+
+   this.state = {
+     monsters: [],
+     searchField: ''
+   };
+
+  //  this.handleChange = this.handleChange.bind(this);
+ }
+
+
+ componentDidMount() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(response => response.json())
+   .then(users => this.setState({ monsters: users }))
+ }
+  
+       
+ handleChnage = e => {
+  this.setState({ searchField : e.target.value });
 }
+
+// handleChnage (e) {
+//   this.setState({ searchField : e.target.value });
+// }
+
+  render() {
+    const {monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()) 
+      );
+
+    return (
+      <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
+         <SearchBox 
+            placeholder="Search Monster"
+            handleChange = {this.handleChnage}
+         />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+/*
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+       {
+         name: 'Frankenstein',
+         id: 'monst1'
+       },
+       {
+         name: 'Dracula',
+         id: 'monst2'
+       },
+       {
+         name: 'Zombie',
+         id: 'monst3'
+       },
+      ]
+    }
+  }
+   
+   render() {
+     return (
+       <div className="App">
+          {
+            this.state.monsters.map( monster => (
+              <h1 key={monster.id}> {monster.name} </h1>
+            ))}
+       </div>
+     );
+   }
+ }
+
+*/ 
+
 
 export default App;
